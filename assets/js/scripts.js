f3cdca39a3b62833dddcad150199620df97546b1
Mário Carvalho <marioacarvalho@gmail.com>
2017-02-10 17:52:17 +0000

    added GA code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,15 @@ jQuery(document).ready(function() {
     */
     $('.top-content').backstretch("assets/img/backgrounds/1.png");
     $('.single-feature-bg').backstretch("assets/img/backgrounds/1.png");
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-73533219-1', 'auto');
+  ga('send', 'pageview');
+
     
 });
 
